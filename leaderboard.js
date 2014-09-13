bc35631290c5031d830245cdc3c2e6e1ba307162
Ryan Orsinger <ryan@ryans-mbp.gateway.2wire.net>
2014-09-13 01:17:34 -0500

    Remove example console.log

--- leaderboard.js
+++ leaderboard.js
@@ -1,9 +1,9 @@
 console.log('This code runs on both server and client');
 
 if(Meteor.isServer) {
-  console.log('Hello Server');
+//
 }
 
 if(Meteor.isClient) {
-  console.log('Hello Client');
+//
 }
